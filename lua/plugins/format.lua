f60484bdafa669c851560754730aff672125f8a0
Edison Chan <edison@ecwk.sh>
2025-06-08 01:46:06 +0800

    Add conform.nvim

---
+++ lua/plugins/format.lua
@@ -0,0 +1,22 @@
+return {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+            }
+        })
+
+        vim.keymap.set({ "n", "v" }, "<leader>p", function()
+            conform.format({
+                formatters_by_ft = {
+                    rust = { "rustfmt" }
+                },
+                lsp_format = "fallback",
+                async = true,
+            })
+        end,  { desc = "Format file" })
+    end
+}
